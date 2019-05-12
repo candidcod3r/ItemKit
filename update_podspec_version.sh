a759bb2f76802cafa1cffdca5d1aa3ef65694c4b
Candid Cod3r <candidcod3r@gmail.com>
2019-05-11 20:41:56 -0700

    Updated podspec to 2.0.1

--- update_podspec_version.sh
+++ update_podspec_version.sh
@@ -3,6 +3,5 @@ git add .; git commit -m "Updated podspec to $1";
 git tag $1;
 git push;
 git push --tags;
-pod lib lint ItemKit.podspec;
 pod repo push SparkPodSpecs ItemKit.podspec;
 pod trunk push ItemKit.podspec --allow-warnings;
